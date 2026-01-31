7be63c09ba2e4f4155b611e44a2fa8acc063b373
Elia Cohen <mrc6113@rit.edu>
2026-01-31 09:37:27 -0500

    constructor for mask class

--- Assets/Scripts/Mask.cs
+++ Assets/Scripts/Mask.cs
@@ -1,10 +1,16 @@
-public abstract class Mask(int row, int column)
+public abstract class Mask
 {
     // The mask's own row
     private int row = row;
 
     // The mask's own column
     private int column = column;
+
+    public Mask(int row, int column)
+    {
+        this.row = row;
+        this.column = column;
+    }
 
     // Activates the masks's special function
     public abstract void Activate(Board<Mask> board);
