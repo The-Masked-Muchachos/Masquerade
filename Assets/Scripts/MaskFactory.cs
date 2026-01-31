76f78ad95f36a53badedbf5569086bf1e5cf5a58
Elia Cohen <mrc6113@rit.edu>
2026-01-31 10:20:52 -0500

    mask factory

---
+++ Assets/Scripts/MaskFactory.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MaskFactory
+{
+    static Mask CreateMaskOfType(char type)
+    {
+        // TODO add mask subclasses
+        switch(type)
+        {
+            case 'S':
+                break;
+            case 'R':
+                break;
+            case 'Y':
+                break;
+            case 'O':
+                break;
+            case 'B':
+                break;
+            case 'G':
+                break;
+            case 'D':
+                break;
+            case 'P':
+                break;
+            case 'W':
+                break;
+            default:
+        }
+    }
+}
