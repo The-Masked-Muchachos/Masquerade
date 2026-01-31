f9241c1e8e0a8757e3216140268579ac9194cc03
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-01-31 11:32:45 -0500

    Mouse Detection now works

--- Assets/Scripts/Mask.cs
+++ Assets/Scripts/Mask.cs
@@ -24,7 +24,7 @@ public abstract class Mask : MonoBehaviour
         Vector2 mousePosInWorld = cam.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 0));
         RaycastHit2D hit = Physics2D.Raycast(mousePosInWorld, Vector2.zero, 0f);
         
-        if (hit.collider && (hit.collider.gameObject.GetInstanceID() == this.gameObject.GetInstanceID()))
+        if (hit.collider && (hit.collider.gameObject.GetInstanceID() == this.gameObject.GetInstanceID()) && Mouse.current.leftButton.wasPressedThisFrame)
         {
             Debug.Log(hit.collider.gameObject.GetInstanceID());
         }
