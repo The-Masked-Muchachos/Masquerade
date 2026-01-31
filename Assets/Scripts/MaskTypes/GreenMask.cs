675f7014b4ded668358a14447d387256f15ac58e
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-01-31 12:24:46 -0500

    started work on green mask

--- Assets/Scripts/MaskTypes/GreenMask.cs
+++ Assets/Scripts/MaskTypes/GreenMask.cs
@@ -1,8 +1,17 @@
 ﻿
+using System.Collections.Generic;
+using System.Numerics;
+
 public class GreenMask : Mask
 {
     public override void Activate(Board board)
     {
+        List<Vector<int>> positions = new List<Vector<int>>();
         
+        // x
+        for (int i = -1; i < 2; i++)
+        {
+            
+        }
     }
 }
