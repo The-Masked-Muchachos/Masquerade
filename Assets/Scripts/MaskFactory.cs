125f29c621c780e5272a642ab2fe9902a205ac67
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-01-31 10:32:04 -0500

    Changes

--- Assets/Scripts/MaskFactory.cs
+++ Assets/Scripts/MaskFactory.cs
@@ -28,5 +28,7 @@ public class MaskFactory
             default:
                 break;
         }
+
+        return null;
     }
 }
---
+++ Assets/Scripts/Objects/MaskObj.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using UnityEngine;
+
+public class MaskObj : MonoBehaviour
+{
+    [SerializeField] private Board board;
+    [SerializeField] private Mask mask;
+
+    public void OnClick()
+    {
+    }
+}
