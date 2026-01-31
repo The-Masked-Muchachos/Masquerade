73b0ea9f091dd8ea74b2abd5284be5ba10278e98
Elia Cohen <mrc6113@rit.edu>
2026-01-31 10:29:44 -0500

    board unity object

---
+++ Assets/Scripts/BoardObj.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class BoardObj : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
