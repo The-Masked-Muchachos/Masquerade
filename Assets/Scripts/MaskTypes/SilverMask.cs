c545c9e27944fd3a04439ca405fea349e7ef80c1
Elia Cohen <mrc6113@rit.edu>
2026-01-31 10:35:46 -0500

    silver mask

---
+++ Assets/Scripts/MaskTypes/SilverMask.cs
@@ -0,0 +1,12 @@
+public class SilverMask : Mask
+{
+    public SilverMask(int row, int column) : base(row, column)
+    {
+        
+    }
+
+    public override void Activate(Board board)
+    {
+        
+    }
+}
