e1cc8a8f8f13a738c42d9510a4ca70bff165241b
Elia Cohen <mrc6113@rit.edu>
2026-01-31 09:33:19 -0500

    mask abstract class

---
+++ Assets/Scripts/Mask.cs
@@ -0,0 +1,13 @@
+using System.Reflection.Metadata;
+
+public abstract class Mask(int row, int column)
+{
+    // The mask's own row
+    private int row = row;
+
+    // The mask's own column
+    private int column = column;
+
+    // Activates the masks's special function
+    public abstract void Activate(Mask[,] board);
+}
