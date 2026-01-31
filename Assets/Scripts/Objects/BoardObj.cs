7582262a3f4ab9f529c5aadec96041f34870b140
Elia Cohen <mrc6113@rit.edu>
2026-01-31 10:38:55 -0500

    board object loads level

--- Assets/Scripts/Objects/BoardObj.cs
+++ Assets/Scripts/Objects/BoardObj.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 
 public class BoardObj : MonoBehaviour
 {
+    Board board;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        board = Board.FromPlaintext("Assets/Levels/temp.txt");
     }
 
     // Update is called once per frame
