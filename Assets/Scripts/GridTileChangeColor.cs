d80e0d46e024a207d3200b437f9323addb492b4a
Elia Cohen <mrc6113@rit.edu>
2026-02-01 14:07:01 -0500

    translucent grid tiles

--- Assets/Scripts/GridTileChangeColor.cs
+++ Assets/Scripts/GridTileChangeColor.cs
@@ -4,11 +4,11 @@ public class GridTileChangeColor : MonoBehaviour
 {
     public void GoYellow()
     {
-        GetComponent<SpriteRenderer>().color = Color.yellow;
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 0f, 0.85f);
     }
 
     public void GoWhite()
     {
-        GetComponent<SpriteRenderer>().color = Color.gray;
+        GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0.5f, 0.85f);
     }
 }
