e39250b79cc12ac0f2aa459aa6be635cc3eb55bb
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-02-23 22:17:31 -0500

    start of a new branch

--- Assets/Scripts/Mask.cs
+++ Assets/Scripts/Mask.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public abstract class Mask : MonoBehaviour
 {
@@ -49,6 +50,9 @@ public abstract class Mask : MonoBehaviour
         }
         float mouseX = Mouse.current.position.x.ReadValue();
         float mouseY = Mouse.current.position.y.ReadValue();
+        
+        
+        
         Vector2 mousePosInWorld = cam.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 0));
         RaycastHit2D hit = Physics2D.Raycast(mousePosInWorld, Vector2.zero, 0f);
 
